7155125980b1bf693736644e0266356b3b6ae62a
[Dev]Zockyyyy || Nico <dev-zockyyyy-nico@github.com>
2020-10-16 17:40:00 +0200

    Update ICommand.java

--- src/main/java/de/nico/commandhandler/ICommand.java
+++ src/main/java/de/nico/commandhandler/ICommand.java
@@ -1,8 +1,10 @@
 package de.nico.commandhandler;
 
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageChannel;
 
 public interface ICommand
 {
-    public void onCommand(Message msg);
+    public void onCommand(Message msg, MessageChannel channel, String[] args);
 }
+    
